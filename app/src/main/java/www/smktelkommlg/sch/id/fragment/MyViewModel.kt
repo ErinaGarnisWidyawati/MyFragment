1f9d2b4342c6410890e4f1d7bb077299630450dc
ErinaGarnisWidyawati <erinagarnis@gmail.com>
2022-04-13 10:51:41 +0700

    Initial commit

---
+++ app/src/main/java/www/smktelkommlg/sch/id/fragment/MyViewModel.kt
@@ -0,0 +1,15 @@
+package www.smktelkommlg.sch.id.fragment
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+
+    private val mName = MutableLiveData<String>()
+    val name: LiveData<String>
+        get() = mName
+    fun setName(name: String) {
+        mName.value = name
+    }
+}
